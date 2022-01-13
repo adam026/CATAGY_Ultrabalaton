b5425912915191825d2a2ec3697e7083556b2c96
adam026 <76121613+adam026@users.noreply.github.com>
2022-01-13 19:45:39 +0100

    Add project files.

---
+++ CATAGY_Ultrabalaton/Futo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CATAGY_Ultrabalaton
+{
+    class Futo
+    {
+        public string Nev { get; set; }
+        public int Rajtszam { get; set; }
+        public bool Kategoria { get; set; }
+        public TimeSpan Ido { get; set; }
+        public int TavSzazalek { get; set; }
+
+        public double IdoOraban => Ido.TotalHours;
+
+        public Futo(string sor)
+        {
+            var buffer = sor.Split(';');
+            Nev = buffer[0];
+            Rajtszam = int.Parse(buffer[1]);
+            Kategoria = buffer[2] == "Ferfi";
+            var ido = buffer[3].Split(':');
+            Ido = new TimeSpan(
+                hours: int.Parse(ido[0]),
+                minutes: int.Parse(ido[1]),
+                seconds: int.Parse(ido[2]));
+            TavSzazalek = int.Parse(buffer[4]);
+
+        }
+    }
+}
---
+++ CATAGY_Ultrabalaton/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CATAGY_Ultrabalaton
+{
+    internal class Program
+    {
+        static List<Futo> futok = new List<Futo>();
+
+        static void Main(string[] args)
+        {
+            Beolvasas();
+            F03();
+            F04();
+            F05();
+            F07();
+            F08();
+
+        }
+
+        private static void F08()
+        {
+            var gyoztesNo = futok.Where(x => !x.Kategoria && x.TavSzazalek == 100).OrderBy(y => y.IdoOraban).First();
+            var gyoztesFerfi = futok.Where(x => x.Kategoria && x.TavSzazalek == 100).OrderBy(y => y.IdoOraban).First();
+
+            Console.WriteLine("8. Feladat: Verseny győztesei");
+            Console.WriteLine($"\tNők: {gyoztesNo.Nev} ({gyoztesNo.Rajtszam}) - {gyoztesNo.Ido}");
+            Console.WriteLine($"\tFérfiak: {gyoztesFerfi.Nev} ({gyoztesFerfi.Rajtszam}) - {gyoztesFerfi.Ido}");
+        }
+
+        private static void F07()
+        {
+            var tttf = futok.Where(x => x.Kategoria && x.TavSzazalek == 100).Average(y => y.IdoOraban);
+            Console.WriteLine($"7. Feladat: Átlagos idő: {tttf} óra");
+        }
+
+        private static void F05()
+        {
+            Console.Write("5. Feladat: Kérem a sportoló nevét: ");
+            var megadottnev = Console.ReadLine();
+            var indultE = futok.Where(x => x.Nev == megadottnev).FirstOrDefault();
+            bool indult = !(indultE is null);
+            Console.WriteLine($"\tIndult egyéniben a sportoló? {(indult ? "Igen" : "Nem")}");
+            if (indult)
+            {
+                Console.WriteLine($"\tTeljesítette a teljes távot? {(indultE.TavSzazalek == 100 ? "Igen" : "Nem")}");
+            }
+        }
+
+        private static void F04()
+        {
+            var teljesTavotTeljesitoNoiSportolok = futok.Where(x => !x.Kategoria && x.TavSzazalek == 100).Count();
+            Console.WriteLine($"4. Feladat: Célba érkező női sportolók: {teljesTavotTeljesitoNoiSportolok} fő");
+        }
+
+        private static void F03()
+        {
+            var egyeniSportolokSzama = futok.Count;
+            Console.WriteLine($"3. Egyéni indulók: {egyeniSportolokSzama} fő");
+        }
+
+        private static void Beolvasas()
+        {
+            using (var sr = new StreamReader(@"..\..\RES\ub2017egyeni.txt", Encoding.UTF8))
+            {
+                var fejlec = sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    futok.Add(new Futo(sr.ReadLine()));
+                }
+            }
+
+        }
+    }
+}
